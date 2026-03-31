14c779d9678b55221561cb64a4eeb300635de2de
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2026-03-31 23:52:31 +0900

    シンタックスエラー修正

--- saba_core/src/renderer/html/token.rs
+++ saba_core/src/renderer/html/token.rs
@@ -7,25 +7,26 @@ use crate::renderer::html::attribute::Attribute;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct HtmlTokenizer {
-    state: State;
-    pos: usize;
-    reconsume: bool;    
-    latest_token: Option<HtmlToken>;
-    input: Vec<char>;
-    buf: String;
+    state: State,
+    pos: usize,
+    reconsume: bool,    
+    latest_token: Option<HtmlToken>,
+    input: Vec<char>,
+    buf: String,
 }
+
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum HtmlToken {
     StartTag {
         tag: String,
-        self)_closing: bool,
+        self_closing: bool,
         attribute: Vec<Attribute>
-    }
+    },
     
     EndTag {
         tag: String,
-    }
+    },
     
     Char(char),
     Eof,
